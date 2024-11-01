821de9b7a172036a62c5fb804d43befb86d5ce01
Kunta-pro <smartedupexa@gmail.com>
2024-11-01 13:46:03 +0300

    Create teacher-dashboard.php

---
+++ php/teacher-dashboard.php
@@ -0,0 +1,6 @@
+<h1>Teacher Dashboard</h1>
+<form method="POST">
+    <input type="text" name="student_id" required placeholder="Student ID">
+    <input type="number" name="marks" required placeholder="Marks">
+    <button type="submit">Upload Marks</button>
+</form>
